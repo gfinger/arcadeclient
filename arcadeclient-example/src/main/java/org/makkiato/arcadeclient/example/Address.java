f338f01c97c900d62376339ecdbbd56686b89e60
Gregor Frey <g.k.frey@gmail.com>
2023-03-23 22:55:03 +0100

    insert embedded documents

--- arcadeclient-example/src/main/java/org/makkiato/arcadeclient/example/Address.java
+++ arcadeclient-example/src/main/java/org/makkiato/arcadeclient/example/Address.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.makkiato.arcadeclient.data.core.DocumentBase;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Address {
+public class Address extends DocumentBase {
     private String street;
     private String zip;
     private String city;
